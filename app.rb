270dbe80deb8abf4112e3290b8bc73fcdedcd6ef
Meghan <meghan.e.mccue@gmail.com>
2020-01-07 14:58:34 +0000

    Add form to enter two player names and /names route to view names

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
+  end
+
+  post '/names' do
+    @player_one = params[:player_one]
+    @player_two = params[:player_two]
+    erb(:play)
   end
 
   run! if app_file == $0
